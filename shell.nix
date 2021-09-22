69dfe0f3a1bb332e36d2e1a32fb374096a01e7b8
Swann Perarnau <swann@anl.gov>
2021-09-22 10:54:39 -0500

    [fix] missing critical env variables in nix shell
    If we want to be able to execute the nrm python code from the shell we
    need to export a few variables in the environment (to locate the
    underlying libraries).

--- shell.nix
+++ shell.nix
@@ -7,5 +7,12 @@ mkShell {
   buildInputs = [
     python3Packages.poetry
     python3Packages.flake8
+    hwloc
+    linuxPackages.perf
   ];
+  shellHook = ''
+    export NRMSO=${nrm-core}/bin/nrm.so
+    export PYNRMSO=${nrm-core}/bin/pynrm.so
+    export PYTHONPATH=.:$PYTHONPATH
+  '';
 }
